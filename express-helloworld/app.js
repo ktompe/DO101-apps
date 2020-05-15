5b8221cd4af16ca81fcb0dfdc090fabdd7791e10
ktompe <tommi.peltola@kela.fi>
2020-05-15 13:28:07 +0300

    whuu

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello W!\n');
+  res.send('Hello Wuf!\n');
 });
 
 app.get('/mars', function (req, res) {
